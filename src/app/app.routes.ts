439b4bbba9192da95952e4590b958e9b0c4548d7
EduardoProfectum <eduardoeandroid@gmail.com>
2021-12-22 10:55:43 -0300

    Componentes Iniciais

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from "@angular/router";
+import { SpotifyComponent } from "./spotify/spotify.component";
+
+
+export const ROUTES: Routes = [
+  {path: 'spotify', component: SpotifyComponent},
+]
---
+++ src/app/spotify/spotify.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tw-spotify',
+  templateUrl: './spotify.component.html'
+})
+export class SpotifyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
