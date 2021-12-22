7c388a9d3f736ead876be853888a2826fe0bbb40
EduardoProfectum <eduardoeandroid@gmail.com>
2021-12-22 13:16:46 -0300

    Criando o Routler-Link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SpotifyComponent } from './spotify/spotify.component';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from "@angular/router";
+
+import { HomeComponent } from "./home/home.component";
 import { SpotifyComponent } from "./spotify/spotify.component";
 
 
 export const ROUTES: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'spotify', component: SpotifyComponent},
 ]
