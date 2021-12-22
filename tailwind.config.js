eff9fd376056260fe1e844b6655afd30d1d531c2
EduardoProfectum <eduardoeandroid@gmail.com>
2021-12-22 16:30:41 -0300

    Layout Spotify

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,32 @@ module.exports = {
   },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '14': '3.5rem',
+        '125px': '125px'
+      },
+      colors: {
+        'hoverspt': '#18D760',
+        'purple-main': '#2D46B9',
+        'green-main': '#1ED760'
+      },
+      backgroundImage: Theme => ({
+        'spotify-theme': "url('assets/img/bursts.svg')",
+        'spotify-theme-mobile': "url('assets/img/bursts-mobile.svg')"
+      }),
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%'
+      },
+      backgroundPosition: {
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center'
+      },
+      fontSize: {
+        '9xl': '9rem'
+      }
+    },
   },
   variants: {
     extend: {},
