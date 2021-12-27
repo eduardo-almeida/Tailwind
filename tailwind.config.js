2b0fa3410e1c351650888e23ebfe45611b2abfa4
EduardoProfectum <eduardoeandroid@gmail.com>
2021-12-27 16:27:44 -0300

    Clone Facebook login

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,23 @@ module.exports = {
     extend: {
       spacing: {
         '14': '3.5rem',
-        '125px': '125px'
+        '30': '7.5rem',
+        '125px': '125px',
+        '290px': '290px',
+        '396px': '396px',
+        '500px': '500px',
+        '980px': '980px'
       },
       colors: {
         'hoverspt': '#18D760',
         'purple-main': '#2D46B9',
-        'green-main': '#1ED760'
+        'green-main': '#1ED760',
+        'gray-main': '#F0F2F5',
+        'blue-main': '#1877F2',
+        'blue-main-hover': '#166FE5',
+        'main-green': '#42B72A',
+        'main-green-hover': '#36A420'
+
       },
       backgroundImage: Theme => ({
         'spotify-theme': "url('assets/img/bursts.svg')",
@@ -30,7 +41,8 @@ module.exports = {
         'banner-mobile': 'top 25% center'
       },
       fontSize: {
-        '9xl': '9rem'
+        '9xl': '9rem',
+        'subtitle': '1.7rem'
       }
     },
   },
